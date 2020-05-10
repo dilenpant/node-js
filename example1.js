96c67d1fe61c136d3eedf5f370fe80aba2a4be59
Dilendra Prasad Pant <dilendraprasadpant@Dilendras-MacBook-Pro.local>
2020-05-10 21:14:37 +0530

    1st node-js helloworld examples

---
+++ example1.js
@@ -0,0 +1,13 @@
+var a = 6
+var  b = 6
+console.log(a+b)
+
+var x = (a,b)=> {
+if(true){
+    console.log('Hi ${a} ${b} ,  '+ a+'  '+b);
+}
+
+}
+
+x('Dilendra', 'Prasad Pant');
+//window.prompt(x);
